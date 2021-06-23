bbf88e545ecd59729e3f50656bc996218235f756
Michel Rodrigo <michel.chagasalves@hotmail.com>
2021-06-23 10:24:46 -0300

    Adicionado o arquivo md

--- pac_sidra.R
+++ pac_sidra.R
@@ -3,6 +3,7 @@
 #' author: "Michel Rodrigo - michel.alves@fjp.mg.gov.br"
 #' date: "22 de junho de 2021"
 #' output: github_document 
+#' always_allow_html: true
 #' ---
 #' 
 #' Importação e manipulação da tabela 1407 do SIDRA - Pesquisa Anual de COmércio - IBGE  
@@ -52,11 +53,88 @@ if (any(pacotes_instalados == FALSE)) {
 lapply(pacotes, library, character.only=TRUE)
 
 #' ## Importação  dos dados
-#' Verifica se os dados já não foi baixados
-entrada  <- if (file.exists("Entrada/tab_1407.csv")) {
-  "Entrada/tab_1407.csv"
+#' Verifica se os dados já não foram baixados
+entrada  <- if (file.exists("tab_1407.csv")) {
+  "tab_1407.csv"
 } else {
   "https://sidra.ibge.gov.br/geratabela?format=us.csv&name=tabela1407.csv&terr=N&rank=-&query=t/1407/n1/all/v/312/p/all/c12354/all/c11066/allxt/l/,,p%2Bt%2Bc12354%2Bv%2Bc11066"
 }
 
-#' ## Manipulação da base de dados
+#' Realiza a importação da tabela
+tab_1407 <- fread(entrada,
+                  integer64 = "numeric",
+                  na.strings = c('"-"','"X"'),
+                  colClasses = c(list("factor" = c(1:5))),
+                  col.names = c("Ano", "Brasil", "Territorio",
+                                "Var", "Divisao", "Valor"),
+                  encoding = "UTF-8")
+
+#' ## Manipulação dos dados
+#' 
+#' ### EXEMPLO 1 
+#' 1 Região, 1 Variável (Pessoal ocupado) e "N" Divisões de comércio 
+#' 
+#' Realiza a filtragem dos dados
+n_divisao <- tab_1407[Territorio %like% "Minas"][, Rank := frank(-Valor, na.last = "keep"), by = Ano]
+
+#' ### EXEMPLO 2 
+
+#' "N" Territórios, 1 Variável(Pessoal ocupado) e 1 Divisão
+
+#' Realiza a filtragem dos dados
+n_territorio <- tab_1407[Territorio %like% "Minas|Janeiro|Paulo|Sudeste" &
+                           Divisao %like% "varejista"]
+
+#' ### EXEMPLO 3 
+#' 
+#' Todos os Estados, 1 Variável, 2 Divisões (em formato wide) e 6 anos
+
+wide_div <- tab_1407[!(Territorio %like% "Brasil|Região") &
+                       Divisao %like% "veículos,|4.4Combustíveis" &
+                       Ano %in% as.factor(2013:2018)] %>%
+  dcast(Ano + Territorio ~ Divisao, value.var = "Valor")
+
+#' Simplifica os nomes de colunas
+colnames(wide_div)[3:4] = c("Comercio.de.veiculos", "Combustiveis")
+
+#' Cria colunas de rank
+wide_div[, `:=` (Rank_V = frank(-Comercio.de.veiculos, na.last = "keep"),
+                 Rank_C = frank(-Combustiveis, na.last = "keep")),
+         by = .(Ano, Territorio)]
+
+
+#' ## Visualização
+
+#' ### Gráfico de bolhas
+#' As 10 maiores Divisao's por Variável, Estado e Ano.
+#' 
+#' CANE's em ordem crescente
+n_divisao$Divisao <- forcats::fct_reorder(n_divisao$Divisao, -n_divisao$Rank)
+
+g_bolha <-  n_divisao[Rank <= 10] %>%
+  ggplot(aes(x = Ano, y = Divisao, text = paste("Rank: ", Rank))) +
+  geom_point(
+    aes(size = Valor, color = as.factor(Rank)),
+    show.legend = F
+  ) +
+  scale_size(range = c(3, 12)) +
+  scale_color_brewer(palette = "Paired") +
+  labs(y = "Divisão de comércio e grupo de atividade") 
+
+# visualizar com o plotly
+ggplotly(g_bolha, tooltip = c("text", "y", "size")) %>% hide_guides()
+
+
+
+#' ## Gráfico de correlação
+
+#' ### Série Histórica
+
+#' elaboração do gráfico
+serie_h <- n_divisao %>%
+  ggplot(aes(x = Ano, y = Valor/1000)) +
+  geom_line(aes(color = Divisao, group = Divisao)) +
+  ylab("Divisão de comércio e grupo de atividade")
+
+# visualizar
+ggplotly(serie_h, tooltip = c("color", "y", "x")) %>% hide_guides()
