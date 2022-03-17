35520671b7f906ece89c704e0f65a30d4ebced46
michelrodrigo <michel.chagasalves@hotmail.com>
2022-03-17 15:45:29 -0300

    Add files via upload

--- Segurança/importacao_dados_IMRS_segurança.R
+++ Segurança/importacao_dados_IMRS_segurança.R
@@ -37,16 +37,16 @@ lapply(pacotes, library, character.only=TRUE)
 source("https://gist.githubusercontent.com/michelrodrigo/d51d98305590f99be1453e7d2424f504/raw/be383ae28291df2a66710868232112c2ae12595b/sim_obt10_prel_mun_mg.R")
 
 #' Ano (o ano relativo aos dados)
-ano <- 2020
+ano_dados <- 2020
 
 #' Realiza a leitura dos arquivos excel 
 dados_pop <- as_tibble(readxl::read_excel("IMRS2021 - BASE DEMOGRAFIA 2000 a 2020.xlsx", sheet =1))
 dados_imrs <- as_tibble(readxl::read_excel("IMRS2021 - BASE SEGURANCA.xlsx", sheet =1))
 
 #' Cria uma cópia dos indicadores do último ano disponível no imrs em seguida atualiza o ano e os valores dos indicadores
-indicadores <- dados_imrs |> subset(ANO == ano)
+indicadores <- dados_imrs |> subset(ANO == ano_dados)
 indicadores[, 5:63] <- NA
-indicadores <- indicadores |> mutate(ANO = ano+1)
+indicadores <- indicadores |> mutate(ANO = ano_dados+1)
 
 #' ## Extração dos dados Datasus
 #' 
@@ -58,7 +58,7 @@ indicadores <- indicadores |> mutate(ANO = ano+1)
 #' P_SUICIDIOS_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                          grande_grupo_cid10 = "X60-X84 Lesões autoprovocadas voluntariamente",
-                         periodo = ano,
+                         periodo = ano_dados,
                          coluna = "Ano do Óbito")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
@@ -68,7 +68,7 @@ indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_S
 #' P_MORTESTRAN_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "V01-V99 Acidentes de transporte",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
@@ -78,7 +78,7 @@ indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_M
 #' P_HOMI_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
@@ -86,7 +86,7 @@ dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOMI_SIM = Total) |> select(-c("Total"))
 
 #' P_HOMITX_SIM
-dados_pop <- dados_pop |> subset(dados_pop$ANO == as.numeric(ano)) |> #seleciona as linhas para o ano de interesse
+dados_pop <- dados_pop |> subset(dados_pop$ANO == as.numeric(ano_dados)) |> #seleciona as linhas para o ano de interesse
 select(c(IBGE6, D_POPTA)) # seleciona as colunas com o código e a população
 
 #' Faz a união da tabela de indicadores com a de população
@@ -98,7 +98,7 @@ indicadores <- indicadores |> mutate(P_HOMITX_SIM = round((P_HOMI_SIM / D_POPTA)
 #' P_HOMMENOR15_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito",
                                faixa_etaria_det = c("0 a 6 dias", "7 a 27 dias", "28 a 364 dias", "Menor 1 ano (ign)", "1 a 4 anos", "5 a 9 anos", "10 a 14 anos"))
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
@@ -109,7 +109,7 @@ indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_H
 #' P_HOM15A24_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito",
                                faixa_etaria_det = c("15 a 19 anos", "20 a 24 anos"))
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
@@ -120,7 +120,7 @@ indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_H
 #' P_HOM25A29_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito",
                                faixa_etaria_det = c("25 a 29 anos"))
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
@@ -131,7 +131,7 @@ indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_H
 #' P_HOMMAIOR30_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito",
                                faixa_etaria_det = c("30 a 34 anos", "35 a 39 anos", "40 a 44 anos", "45 a 49 anos", "50 a 54 anos", "55 a 59 anos", "60 a 64 anos", "65 a 69 anos", "70 a 74 anos", "75 a 79 anos", "80 anos e mais"))
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
@@ -142,7 +142,7 @@ indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_H
 #' P_HOMBRANCA_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito",
                                cor_raca = "Branca")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
@@ -153,7 +153,7 @@ indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_H
 #' P_HOMPRETA_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito",
                                cor_raca = "Preta")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
@@ -164,7 +164,7 @@ indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_H
 #' P_HOMPARDA_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito",
                                cor_raca = "Parda")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
@@ -175,7 +175,7 @@ indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_H
 #' P_HOMOUTROS_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito",
                                cor_raca = c("Indígena", "Amarela", "Ignorado"))
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
@@ -186,7 +186,7 @@ indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_H
 #' P_HOMHOMEM_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito",
                                sexo = "Masc")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
@@ -197,77 +197,86 @@ indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_H
 #' P_HOMMULHER_SIM
 dados <- sim_obt10_prel_mun_mg(linha = "Município",
                                grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano,
+                               periodo = ano_dados,
                                coluna = "Ano do Óbito",
                                sexo = "Fem")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOMMULHER_SIM = Total) |> select(-c("Total"))
+indicadores <- 
 
 #' ## Extração dos dados SEJUSP
 
 #' Obtém os dados disponiveis na página de dados abertos
-#pag_sejusp <- read_html("http://www.seguranca.mg.gov.br/2018-08-22-13-39-06/dados-abertos")
-#url <- pag_sejusp %>% html_element(xpath = "//a[preceding-sibling::*[text()[contains(., '2012 a 2017')]]]") %>% html_attr(name = 'href')
-#dados_sejusp <- import(paste0("http://www.seguranca.mg.gov.br", url) )
+pag_sejusp <- read_html("http://www.seguranca.mg.gov.br/2018-08-22-13-39-06/dados-abertos")
+url <- pag_sejusp %>% html_element(xpath = "//a[preceding-sibling::*[text()[contains(., '2012 a 2017')]]]") %>% html_attr(name = 'href')
+dados_sejusp <- import(paste0("http://www.seguranca.mg.gov.br", url) )
 
-dados_sejusp <- as_tibble(read.csv("Banco Crimes Violentos - Atualizado Dezembro 2021.csv", sep = ";" ))
+#dados_sejusp <- as_tibble(read.csv("Banco Crimes Violentos - Atualizado Dezembro 2021.csv", sep = ";" ))
+
+dados_sejusp <- dados_sejusp |> janitor::clean_names()
 
 
 #' P_CV
-dados <- dados_sejusp |> subset(Ano == ano) |> group_by(Ano,`Cod.IBGE`) |> summarise(Total = sum(Registros), .groups = "keep") 
+dados <- dados_sejusp |> subset(ano == ano_dados) |> group_by(ano, cod_ibge) |> summarise(Total = sum(registros), .groups = "keep") 
 colnames(dados) <- c("Ano", "IBGE6", "Total")
 indicadores <- indicadores |> merge(dados[, c(2, 3)], by="IBGE6") |>  mutate(P_CV = round((Total / D_POPTA) * 100000, digits = 2)) |> select(-c("Total"))
 
 #' P_CVPA
-dados <- dados_sejusp %>% group_by(Ano,`Cod.IBGE`) %>% 
-                          subset(Ano == ano) %>%
-                          subset(Natureza == "Roubo Consumado" | Natureza == "Extorsão Mediante Sequestro Consumado") %>% 
-                          summarise(Total = sum(Registros), .groups = "keep") 
+dados <- dados_sejusp %>% group_by(ano,cod_ibge) %>% 
+                          subset(dados_sejusp$ano == ano_dados) %>%
+                          subset(natureza == "Roubo Consumado" | natureza == "Extorsão Mediante Sequestro Consumado") %>% 
+                          summarise(Total = sum(registros), .groups = "keep") 
 colnames(dados) <- c("Ano", "IBGE6", "Total")
 indicadores <- indicadores |> merge(dados[, c(2, 3)], by="IBGE6") |>  mutate(P_CVPA = round((Total / D_POPTA) * 100000, digits = 2)) |> select(-c("Total"))
 
 #' P_CVPE
-dados <- dados_sejusp %>% group_by(Ano,`Cod.IBGE`) %>% 
-                          subset(Ano == ano) %>%
-                          subset(Natureza == "Homicídio Consumado (Registros)" | Natureza == "Homicídio Tentado" | Natureza == "Estupro Consumado" | Natureza == "Estupro Tentado" | Natureza == "Estupro de Vulnerável Consumado" | Natureza == "Estupro de Vulnerável Tentado") %>% 
-                          summarise(Total = sum(Registros), .groups = "keep") 
-colnames(dados) <- c("Ano", "IBGE6", "Total")
-indicadores <- indicadores |> merge(dados[, c(2, 3)], by="IBGE6") |>  mutate(P_CVPA = round((Total / D_POPTA) * 100000, digits = 2)) |> select(-c("Total"))
+dados <- dados_sejusp %>% group_by(dados_sejusp$ano,cod_ibge) %>% 
+                          subset(dados_sejusp$ano == ano_dados) %>%
+                          subset(natureza == "Homicídio Consumado (Registros)" | natureza == "Homicídio Tentado" | natureza == "Estupro Consumado" | natureza == "Estupro Tentado" | natureza == "Estupro de Vulnerável Consumado" | natureza == "Estupro de Vulnerável Tentado") %>% 
+                          summarise(Total = sum(registros), .groups = "keep") 
+colnames(dados) <- c("ano", "IBGE6", "Total")
+indicadores <- indicadores |> merge(dados[, c(2, 3)], by="IBGE6") |>  mutate(P_CVPE = round((Total / D_POPTA) * 100000, digits = 2)) |> select(-c("Total"))
 
 #' P_HOM_TX e P_HOMI
-dados <- dados_sejusp %>% group_by(Ano,`Cod.IBGE`) %>% 
-                          subset(Ano == ano) %>%
-                          subset(Natureza == "Homicídio Consumado (Registros)") %>%
-                          summarise(Total = sum(Registros), .groups = "keep") 
-colnames(dados) <- c("Ano", "IBGE6", "Total")
+dados <- dados_sejusp %>% group_by(dados_sejusp$ano,cod_ibge) %>% 
+                          subset(dados_sejusp$ano == ano_dados) %>%
+                          subset(natureza == "Homicídio Consumado (Registros)") %>%
+                          summarise(Total = sum(registros), .groups = "keep") 
+colnames(dados) <- c("ano", "IBGE6", "Total")
 indicadores <- indicadores |> merge(dados[, c(2, 3)], by="IBGE6") |>  mutate(P_HOM_TX = round((Total / D_POPTA) * 100000, digits = 2)) |>
                                                                       mutate(P_HOMI = round(Total, digits = 2)) |> select(-c("Total"))
 
 #' P_TEN_HOM
-dados <- dados_sejusp %>% group_by(Ano,`Cod.IBGE`) %>% 
-                          subset(Ano == ano) %>%
-                          subset(Natureza == "Homicídio Tentado") %>% 
-                          summarise(Total = sum(Registros), .groups = "keep") 
-colnames(dados) <- c("Ano", "IBGE6", "Total")
+dados <- dados_sejusp %>% group_by(dados_sejusp$ano,cod_ibge) %>% 
+                          subset(dados_sejusp$ano == ano_dados) %>%
+                          subset(natureza == "Homicídio Tentado") %>% 
+                          summarise(Total = sum(registros), .groups = "keep") 
+colnames(dados) <- c("ano", "IBGE6", "Total")
 indicadores <- indicadores |> merge(dados[, c(2, 3)], by="IBGE6") |> mutate(P_TEN_HOM = round(Total, digits = 2)) |> select(-c("Total"))
 
 #' P_ROUBO
-dados <- dados_sejusp %>% group_by(Ano,`Cod.IBGE`) %>% 
-                          subset(Ano == ano) %>%
-                          subset(Natureza == "Roubo Consumado") %>%
-                          summarise(Total = sum(Registros), .groups = "keep") 
-colnames(dados) <- c("Ano", "IBGE6", "Total")
+dados <- dados_sejusp %>% group_by(dados_sejusp$ano,cod_ibge) %>% 
+                          subset(dados_sejusp$ano == ano_dados) %>%
+                          subset(natureza == "Roubo Consumado") %>%
+                          summarise(Total = sum(registros), .groups = "keep") 
+colnames(dados) <- c("ano", "IBGE6", "Total")
 indicadores <- indicadores |> merge(dados[, c(2, 3)], by="IBGE6") |> mutate(P_ROUBO = round(Total, digits = 2)) |> select(-c("Total"))
 
 #' P_ESTUPRO
-dados <- dados_sejusp %>% group_by(Ano,`Cod.IBGE`) %>% 
-                          subset(Ano == ano) %>%
-                          subset(Natureza == "Estupro Consumado" | Natureza == "Estupro de Vulnerável Consumado" | Natureza == "Estupro de Vulnerável Tentado" | Natureza == "Estupro Tentado") %>%
-                          summarise(Total = sum(Registros), .groups = "keep") 
-colnames(dados) <- c("Ano", "IBGE6", "Total")
+dados <- dados_sejusp %>% group_by(dados_sejusp$ano,cod_ibge) %>% 
+                          subset(dados_sejusp$ano == ano_dados) %>%
+                          subset(natureza == "Estupro Consumado" | natureza == "Estupro de Vulnerável Consumado" | natureza == "Estupro de Vulnerável Tentado" | natureza == "Estupro Tentado") %>%
+                          summarise(Total = sum(registros), .groups = "keep") 
+colnames(dados) <- c("ano", "IBGE6", "Total")
 indicadores <- indicadores |> merge(dados[, c(2, 3)], by="IBGE6") |> mutate(P_ESTUPRO = round(Total, digits = 2)) |> select(-c("Total", "D_POPTA"))
 
+
+#' Faz a união dos dados originais do IMRS com os dados calculados para o ano atual
+dados_imrs <- rbind(dados_imrs, indicadores)
+
+#' Salva o resultado num novo arquivo excel
+write_xlsx(dados_imrs, paste0("IMRS", as.character(ano_dados+2), " - BASE SEGURANÇA.xlsx"))
 
 
