c201afbef797b93e1869517d99b9e1f6459b39ca
michelrodrigo <michel.chagasalves@hotmail.com>
2022-04-28 09:47:47 -0300

    Update importacao_dados_IMRS_segurança.R
    Nessa nova versão é possível escolher entre os dados preliminares e os consolidados do DATASUS. A função sim_obt10_mun_mg fará uma verificação da disponibilidade dos dados.

--- Segurança/importacao_dados_IMRS_segurança.R
+++ Segurança/importacao_dados_IMRS_segurança.R
@@ -34,10 +34,11 @@ if (any(pacotes_instalados == FALSE)) {
 lapply(pacotes, library, character.only=TRUE)
 
 #' Busca a função que faz a extração dos dados do datasus diretamente do github
-source("https://gist.githubusercontent.com/michelrodrigo/d51d98305590f99be1453e7d2424f504/raw/be383ae28291df2a66710868232112c2ae12595b/sim_obt10_prel_mun_mg.R")
+source("https://gist.githubusercontent.com/michelrodrigo/c19a28180ee0aa4d589eddbf7038b413/raw/b0cc8c7aba5b674b3a051b5f8e60d110b4136e10/sim_obt10_mun_mg.R")
 
 #' Ano (o ano relativo aos dados)
 ano_dados <- 2020
+dados_preliminares <- FALSE
 
 #' Realiza a leitura dos arquivos excel 
 dados_pop <- as_tibble(readxl::read_excel("IMRS2021 - BASE DEMOGRAFIA 2000 a 2020.xlsx", sheet =1))
@@ -54,9 +55,12 @@ indicadores <- indicadores |> mutate(ANO = ano_dados+1)
 #' faz-se a união dos dados obtidos com a tabela de indicadores pelo código do ibge, faz-se a transformação do valor
 #' para o formato numérico e finalmente faz-se a atualização da coluna correspondente ao indicador.
 #' 
+#' 
+
 
 #' P_SUICIDIOS_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                         linha = "Município",
                          grande_grupo_cid10 = "X60-X84 Lesões autoprovocadas voluntariamente",
                          periodo = ano_dados,
                          coluna = "Ano do Óbito")
@@ -66,20 +70,22 @@ dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_SUICIDIOS_SIM = Total) |> select(-c("Total"))
 
 #' P_MORTESTRAN_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "V01-V99 Acidentes de transporte",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito")
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                          linha = "Município",
+                          grande_grupo_cid10 = "V01-V99 Acidentes de transporte",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_MORTESTRAN_SIM = Total) |> select(-c("Total"))
 
 #' P_HOMI_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito")
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                          linha = "Município",
+                          grande_grupo_cid10 = "X85-Y09 Agressões",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
@@ -96,122 +102,140 @@ indicadores <- merge(indicadores, dados_pop, by="IBGE6")
 indicadores <- indicadores |> mutate(P_HOMITX_SIM = round((P_HOMI_SIM / D_POPTA) * 100000, digits = 2)) 
 
 #' P_HOMMENOR15_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito",
-                               faixa_etaria_det = c("0 a 6 dias", "7 a 27 dias", "28 a 364 dias", "Menor 1 ano (ign)", "1 a 4 anos", "5 a 9 anos", "10 a 14 anos"))
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                          linha = "Município",
+                          grande_grupo_cid10 = "X85-Y09 Agressões",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito",
+                          faixa_etaria_det = c("0 a 6 dias", "7 a 27 dias", "28 a 364 dias", "Menor 1 ano (ign)", "1 a 4 anos", "5 a 9 anos", "10 a 14 anos"))
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOMMENOR15_SIM = Total) |> select(-c("Total"))
 
 #' P_HOM15A24_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito",
-                               faixa_etaria_det = c("15 a 19 anos", "20 a 24 anos"))
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                          linha = "Município",
+                          grande_grupo_cid10 = "X85-Y09 Agressões",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito",
+                          faixa_etaria_det = c("15 a 19 anos", "20 a 24 anos"))
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOM15A24_SIM = Total) |> select(-c("Total"))
 
 #' P_HOM25A29_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito",
-                               faixa_etaria_det = c("25 a 29 anos"))
+dados <- sim_obt10_mun_mg(dados_preliminares, linha = "Município",
+                          grande_grupo_cid10 = "X85-Y09 Agressões",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito",
+                          faixa_etaria_det = c("25 a 29 anos"))
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOM25A29_SIM = Total) |> select(-c("Total"))
 
 #' P_HOMMAIOR30_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito",
-                               faixa_etaria_det = c("30 a 34 anos", "35 a 39 anos", "40 a 44 anos", "45 a 49 anos", "50 a 54 anos", "55 a 59 anos", "60 a 64 anos", "65 a 69 anos", "70 a 74 anos", "75 a 79 anos", "80 anos e mais"))
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                          linha = "Município",
+                          grande_grupo_cid10 = "X85-Y09 Agressões",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito",
+                          faixa_etaria_det = c("30 a 34 anos", "35 a 39 anos", "40 a 44 anos", "45 a 49 anos", "50 a 54 anos", "55 a 59 anos", "60 a 64 anos", "65 a 69 anos", "70 a 74 anos", "75 a 79 anos", "80 anos e mais"))
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOMMAIOR30_SIM = Total) |> select(-c("Total"))
 
 #' P_HOMBRANCA_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito",
-                               cor_raca = "Branca")
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                          linha = "Município",
+                          grande_grupo_cid10 = "X85-Y09 Agressões",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito",
+                          cor_raca = "Branca")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOMBRANCA_SIM = Total) |> select(-c("Total"))
 
 #' P_HOMPRETA_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito",
-                               cor_raca = "Preta")
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                          linha = "Município",
+                          grande_grupo_cid10 = "X85-Y09 Agressões",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito",
+                          cor_raca = "Preta")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOMPRETA_SIM = Total) |> select(-c("Total"))
 
 #' P_HOMPARDA_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito",
-                               cor_raca = "Parda")
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                          linha = "Município",
+                          grande_grupo_cid10 = "X85-Y09 Agressões",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito",
+                          cor_raca = "Parda")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOMPARDA_SIM = Total) |> select(-c("Total"))
 
 #' P_HOMOUTROS_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito",
-                               cor_raca = c("Indígena", "Amarela", "Ignorado"))
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                          linha = "Município",
+                          grande_grupo_cid10 = "X85-Y09 Agressões",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito",
+                          cor_raca = c("Indígena", "Amarela", "Ignorado"))
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOMOUTROS_SIM = Total) |> select(-c("Total"))
 
 #' P_HOMHOMEM_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito",
-                               sexo = "Masc")
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                          linha = "Município",
+                          grande_grupo_cid10 = "X85-Y09 Agressões",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito",
+                          sexo = "Masc")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOMHOMEM_SIM = Total) |> select(-c("Total"))
 
 #' P_HOMMULHER_SIM
-dados <- sim_obt10_prel_mun_mg(linha = "Município",
-                               grande_grupo_cid10 = "X85-Y09 Agressões",
-                               periodo = ano_dados,
-                               coluna = "Ano do Óbito",
-                               sexo = "Fem")
+dados <- sim_obt10_mun_mg(dados_preliminares,
+                          linha = "Município",
+                          grande_grupo_cid10 = "X85-Y09 Agressões",
+                          periodo = ano_dados,
+                          coluna = "Ano do Óbito",
+                          sexo = "Fem")
 dados <- dados |> mutate(IBGE6 = apply(dados, 1, function(x) substr(x["Município"], 1, 6) ))
 dados[, c(3, 4)] <- sapply(dados[, c(3, 4)], as.numeric)
 
 indicadores <- indicadores |> merge(dados[, c(3, 4)], by="IBGE6") |>  mutate(P_HOMMULHER_SIM = Total) |> select(-c("Total"))
-indicadores <- 
+
 
 #' ## Extração dos dados SEJUSP
 
 #' Obtém os dados disponiveis na página de dados abertos
 pag_sejusp <- read_html("http://www.seguranca.mg.gov.br/2018-08-22-13-39-06/dados-abertos")
 url <- pag_sejusp %>% html_element(xpath = "//a[preceding-sibling::*[text()[contains(., '2012 a 2017')]]]") %>% html_attr(name = 'href')
-dados_sejusp <- import(paste0("http://www.seguranca.mg.gov.br", url) )
+url <- paste0("http://www.seguranca.mg.gov.br", url)
+p <- curlGetHeaders(url)
+status <- attr(p, "status")
+if(status == 503){
+  stop("Dados da SEJUSP não disponíveis")
+}else if(status == 200){
+  # apesar de termos obtido uma resposta, ela pode ser uma resposta indicando um erro. A seguir tentaremos ler os dados
+  p <- read_html(url)
+}
+
 
 #dados_sejusp <- as_tibble(read.csv("Banco Crimes Violentos - Atualizado Dezembro 2021.csv", sep = ";" ))
 
